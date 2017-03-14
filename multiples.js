4b50eff12104a41e003a2d02b8aead9cb72932ae
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2017-03-14 09:42:55 -1000

    finished challenge

--- multiples.js
+++ multiples.js
@@ -7,6 +7,18 @@ exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
   // do your work here
+  for(var x = 0; x < n; x++){
+  	if(x % 3 === 0 || x % 5 === 0){
+  		sum += x;
+  //I want to store the multiples into something to retrieve it later
+  		
+  //I want to access these stored multiples and add them all up
+
+
+  	}
+  }
+  console.log(sum);
 
   return sum;
 };
+exports.sumOfAMultiple(100);
